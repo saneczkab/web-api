7f232217764545768556d143b03f43b99104bdeb
Alexandr Iarmoshenko <a2398253@gmail.com>
2025-09-30 16:30:11 +0500

    Task 1 done

--- WebApi.MinimalApi/Controllers/UsersController.cs
+++ WebApi.MinimalApi/Controllers/UsersController.cs
@@ -1,22 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using WebApi.MinimalApi.Domain;
 using WebApi.MinimalApi.Models;
 
 namespace WebApi.MinimalApi.Controllers;
 
+[Produces("application/json", "application/xml")]
 [Route("api/[controller]")]
 [ApiController]
 public class UsersController : Controller
 {
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+    
     // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-    public UsersController(IUserRepository userRepository)
+    public UsersController(IUserRepository userRepository, IMapper mapper)
     {
+        _userRepository = userRepository;
+        _mapper = mapper;
     }
 
     [HttpGet("{userId}")]
     public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
     {
-        throw new NotImplementedException();
+        var user = _userRepository.FindById(userId);
+        if (user == null)
+            return NotFound();
+        
+        var userDto = _mapper.Map<UserDto>(user);
+        
+        return userDto;
     }
 
     [HttpPost]
